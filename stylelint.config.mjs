fb899b3cd8d0f44d79de729182feba23f5660ca9
dipeng <dipeng@urlinsgroup.com>
2025-09-19 12:12:23 -0400

    fixed stylelint errs

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard"]
+  "extends": ["stylelint-config-standard", "stylelint-config-tailwindcss"],
+  rules: {
+    "import-notation": "string",
+    // "at-rule-no-unknown": [true, { ignoreAtRules: ["theme", "utility", "layer", "custom-variant"] }],
+  },
 };
