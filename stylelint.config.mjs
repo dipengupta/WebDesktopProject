f57caa80a7112a5f2a8f257835f16f09cf6ca646
dipeng <dipeng@urlinsgroup.com>
2025-09-19 12:33:15 -0400

    added prettier

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard", "stylelint-config-tailwindcss"],
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
   rules: {
-    "import-notation": "string",
+    'import-notation': 'string'
     // "at-rule-no-unknown": [true, { ignoreAtRules: ["theme", "utility", "layer", "custom-variant"] }],
-  },
+  }
 };
