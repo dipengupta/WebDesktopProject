2497088a611ad38a47c6b2aa56bd50866d25a002
dipeng <dipeng@urlinsgroup.com>
2025-09-22 14:26:57 -0400

    added husky and lint-staged

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,41 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-tailwindcss',
+    'stylelint-config-sass-guidelines'
+  ],
   rules: {
-    'import-notation': 'string'
-    // "at-rule-no-unknown": [true, { ignoreAtRules: ["theme", "utility", "layer", "custom-variant"] }],
+    'import-notation': 'string',
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'layer',
+          'config',
+          'variants',
+          'responsive',
+          'screen',
+          'theme' // Tailwind v4
+        ]
+      }
+    ],
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'layer',
+          'config',
+          'variants',
+          'responsive',
+          'screen',
+          'theme'
+        ]
+      }
+    ]
   }
 };
